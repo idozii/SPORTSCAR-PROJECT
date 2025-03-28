810f3616ecace27fecf392ecf9a8a383376e9353
idozii <tue.nguyenpc0401@hcmut.edu.vn>
2025-03-28 15:23:39 +0700

    done format data

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import matplotlib.pyplot as plt
 datapath = 'data/Elite Sports Cars in Data.csv'
 data = pd.read_csv(datapath)
 
-# Categorical: Brand, Model, Country, Market_Demand, Modification
-# Numerical: Year, Engine_Size, Condition, Transmission, Popularity, Fuel_Type, Drivetrain, Horsepower, Torque, Weight, Top_Speed, Acceleration_0_100, Fuel_Efficiency, CO2_Emissions, Price, Mileage, Safety_Rating, Number_of_Owners, Insurance_Cost, Production_Units, Log_Price, Log_Mileage
+# Categorical: Brand, Model, Country, Modification
+# Numerical: Year, Engine_Size, Condition, Transmission, Market_Demand, Popularity, Fuel_Type, Drivetrain, Horsepower, Torque, Weight, Top_Speed, Acceleration_0_100, Fuel_Efficiency, CO2_Emissions, Price, Mileage, Safety_Rating, Number_of_Owners, Insurance_Cost, Production_Units, Log_Price, Log_Mileage
 # Null in Modification
 
 # Data Cleaning
@@ -41,6 +41,11 @@ data['Transmission'] = data['Transmission'].apply(lambda x: 0 if x == 'cvt' else
 data['Popularity'] = data['Popularity'].str.lower()
 data['Popularity'] = data['Popularity'].apply(lambda x: 0 if x == 'low' else (1 if x == 'medium' else 2))
 
+## Format Market_Demand
+### 0: Low, 1: Medium, 2: High
+data['Market_Demand'] = data['Market_Demand'].str.lower()
+data['Market_Demand'] = data['Market_Demand'].apply(lambda x: 0 if x == 'low' else (1 if x == 'medium' else 2))
+
 print(data.head())
 print(data.info())
-print(data['Market_Demand'].value_counts())
+print(data['Modification'].value_counts())
